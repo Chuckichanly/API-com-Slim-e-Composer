394dd66e80317451d10b5b1216d37182ffb1d314
Vitor Dilucca <vitordilucca@hotmail.com>
2022-10-28 20:16:35 -0300

    535 20:18 api de usuarios e de postagens

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require './vendor/autoload.php';
-use \Psr\Http\Message\ServerRequestInterface as Request;
+/* use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
  
  
@@ -8,8 +8,19 @@ $app = new \Slim\App;
 $app->get('/postagens/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
     $response->getBody()->write("Hello, $name");
- 
+    
     return $response;
+}); */
+
+$app = new \Slim\App;
+
+$app->get('/postagens', function(){
+    echo "Listagem de postagens";
 });
+
+$app->get('/usuarios', function(){
+    echo "Listagem de usuarios";
+});
+
  
 $app->run();
